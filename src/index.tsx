5d2574824576f70e8b166b5ea958812e1ab93af0
ushironoko <apple19940820@yahoo.co.jp>
2019-01-19 23:07:31 +0900

    rename

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import Index from './pages/index'
-import './App.css'
+import './Index.css'
 
 ReactDOM.render(<Index />, document.querySelector('#root'))
